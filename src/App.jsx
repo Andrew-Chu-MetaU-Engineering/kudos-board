55fc997c96d31192adf16beef08ab239dfa9a67a
Andrew Chu <andrewchu@meta.com>
2024-06-18 09:21:31 -0700

    board modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import BoardCard from "./BoardCard";
 import OptionsBanner from "./OptionsBanner";
+import BoardModal from "./BoardModal";
 
 export default function App() {
   const [boardData, setBoardData] = useState([
@@ -10,32 +11,42 @@ export default function App() {
       title: "a",
       description: "celebration board",
       category: "celebration",
+      image: "image0",
+      author: "author0",
     },
     {
       id: 1,
       title: "b",
       description: "thank you board",
       category: "thank-you",
+      image: "image1",
+      author: "author1",
     },
     {
       id: 2,
       title: "c",
       description: "inspiration board",
       category: "inspiration",
+      image: "image2",
+      author: "author2",
     },
     {
       id: 3,
       title: "d",
       description: "thank you board 2",
       category: "thank-you",
+      image: "image3",
+      author: "author3",
     },
   ]);
   const [searchedBoards, setSearchedBoards] = useState(boardData);
   const [displayedBoards, setDisplayedBoards] = useState(searchedBoards);
   const [filterOption, setFilterOption] = useState("all");
   const [searchQuery, setSearchQuery] = useState("");
+  const [displayBoardModal, setDisplayBoardModal] = useState(false);
 
   useEffect(() => {
+    console.log(boardData);
     if (searchQuery) {
       setSearchedBoards(
         boardData.filter((board) =>
@@ -68,7 +79,7 @@ export default function App() {
   }
 
   function handleAddBoard() {
-    // TODO implement add board modal
+    setDisplayBoardModal(true);
   }
 
   function handleOpenBoard(id) {
@@ -83,6 +94,13 @@ export default function App() {
       setBoardData(boardData.filter((board) => board.id !== id));
       setDisplayedBoards(displayedBoards.filter((board) => board.id !== id));
     };
+  }
+  function handleBoardCreation(e) {
+    e.preventDefault();
+
+    const form = e.target;
+    const formData = new FormData(form);
+    setBoardData([...boardData, { ...formData }]); // TODO unique key prop!
   }
 
   return (
@@ -103,6 +121,14 @@ export default function App() {
           handleDeleteBoard={handleDeleteBoard}
         />
       ))}
+
+      {displayBoardModal && (
+        <BoardModal
+          handleBoardCreation={handleBoardCreation}
+          displayBoardModal={displayBoardModal}
+          setDisplayBoardModal={setDisplayBoardModal}
+        />
+      )}
     </>
   );
 }
---
+++ src/BoardModal.jsx
@@ -0,0 +1,52 @@
+import PropTypes from "prop-types";
+import {
+  Button,
+  Modal,
+  TextField,
+  Select,
+  MenuItem,
+  InputLabel,
+  FormControl,
+} from "@mui/material";
+
+import "./BoardModal.css";
+
+export default function BoardModal({
+  handleBoardCreation,
+  displayBoardModal,
+  setDisplayBoardModal,
+}) {
+  return (
+    <Modal
+      open={displayBoardModal}
+      onClose={() => setDisplayBoardModal(false)}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <form id="modal-form" onSubmit={handleBoardCreation}>
+        <TextField required name="title" label="Title" />
+        <TextField required name="description" label="Description" />
+        <TextField required name="image" label="Image" />
+        <TextField name="author" label="Author" />
+
+        <FormControl fullWidth>
+          <InputLabel>Category *</InputLabel>
+          <Select name="category" defaultValue="">
+            <MenuItem value="celebration">Celebration</MenuItem>
+            <MenuItem value="thank-you">Thank You</MenuItem>
+            <MenuItem value="inspiration">Inspiration</MenuItem>
+          </Select>
+        </FormControl>
+        <Button type="submit" variant="outlined">
+          Create Board
+        </Button>
+      </form>
+    </Modal>
+  );
+}
+
+BoardModal.propTypes = {
+  handleBoardCreation: PropTypes.func.isRequired,
+  displayBoardModal: PropTypes.bool.isRequired,
+  setDisplayBoardModal: PropTypes.func.isRequired,
+};
